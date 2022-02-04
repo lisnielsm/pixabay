c4aa9cc8eca60aa5011aa5b6fc1406a9bcda4906
Lisniel Sanchez Morales <lisnielsm@gmail.com>
2022-02-04 08:46:44 -0500

    Se realizo la busqueda del termino a la API y se guardo los resultados en un state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [busqueda, guardarBusqueda] = useState('');
+  const [imagenes, guardarImagenes] = useState([]);
+
+  useEffect(() => {
+
+    const consultarAPI = async () => {
+      if (busqueda === '') return;
+
+      const imagenesPorPagina = 10;
+      const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarImagenes(resultado.hits);
+    };
+    consultarAPI();
+  }, [busqueda]);
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imágenes</p>
 
-        <Formulario />
+        <Formulario
+          guardarBusqueda={guardarBusqueda}
+        />
       </div>
 
     </div>
